6e179b47ede89b5790d2ef38e188671d6f99fad4
Lincoml Rufino <linconldasilvarufino@gmail.com>
2022-08-24 16:16:45 -0300

    FormGroup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Todo } from 'src/model/Todo.model';
 
 @Component({
@@ -8,11 +9,36 @@ import { Todo } from 'src/model/Todo.model';
 })
 export class AppComponent {
   public todos: Todo[] = [];
-  public title: string = "Minhas Tarefas"
-  constructor() {
+  public title: string = "Minhas Tarefas";
+  public form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.max(60),
+        Validators.required
+      ])]
+    });
+
     this.todos.push(new Todo(1, "Estudar", false))
     this.todos.push(new Todo(2, "Dormir", false))
     this.todos.push(new Todo(3, "Comer", false))
-    this.todos.push(new Todo(4, "Perder", true))
+    this.todos.push(new Todo(4, "Perder no CS", false))
+  }
+
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+
+    if (index !== -1)
+      this.todos.splice(index, 1);
+  }
+
+  markAsDone(todo: Todo) {
+    todo.done = true;
+  }
+
+  markAsUndone(todo: Todo) {
+    todo.done = false;
   }
 }
